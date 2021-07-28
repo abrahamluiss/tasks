c869780526d692d3e597fb93208d28b673fdd910
Abraham Saldana <abraham105@hotmail.com>
2021-07-28 11:25:22 -0500

    add task table

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -17,7 +17,7 @@ class UsersTableSeeder extends Seeder
         DB::table('users')->insert([
             'id' => 1,
             'name' => 'Admin Admin',
-            'email' => 'admin@black.com',
+            'email' => 'admin@admin.com',
             'email_verified_at' => now(),
             'password' => Hash::make('secret'),
             'created_at' => now(),
